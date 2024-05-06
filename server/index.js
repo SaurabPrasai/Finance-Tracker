1478eb2b38eeeb42b453221d052022b50d04d289
Saurab Prasai <saurablovetocode@gmail.com>
2024-05-06 16:31:01 +0545

    create db connection and financial record schema

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 import 'dotenv/config'
 import express from "express"
+import mongoose from 'mongoose';
 
 const app=express();
 
-
-app.listen(3000,()=>{
-    console.log("Server running on port "+3000);
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    app.listen(3000,()=>{
+        console.log("Server running on port "+3000);
+    })
 })
+.catch((err)=>{
+    console.log(err);
+})
+
+// middleware
+app.use(express.json());
---
+++ server/models/financialRecord.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const financialSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  payment: {
+    type: String,
+    required: true,
+  },
+});
+
+const FinancialRecordModel=mongoose.model("FinancialRecord",financialSchema)
+
+export default FinancialRecordModel
