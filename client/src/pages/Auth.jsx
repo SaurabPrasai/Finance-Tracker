3342cc451b2a65b37450632e4e19e22f9b55f6ba
Saurab Prasai <saurablovetocode@gmail.com>
2024-05-06 15:26:54 +0545

    complete clerk signup and signin functionality

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { SignInButton, SignUpButton, SignedOut,SignedIn,UserButton } from "@clerk/clerk-react"
 
 export default function Auth() {
   return (
-    <div>Auth</div>
+    <div className="">
+        <SignedOut>
+            <SignUpButton  mode={"modal"}className=" border-2 border-gray-600 p-3 rounded shadow-sm bg-gray-300  hover:bg-opacity-95 m-4"/>
+            <SignInButton mode={"modal"} className=" border-2  border-gray-600 p-3 rounded shadow-sm bg-gray-300  hover:bg-opacity-95 m-4"/>
+        </SignedOut>
+        <SignedIn>
+            <UserButton/>
+        </SignedIn>
+    </div>
   )
 }
