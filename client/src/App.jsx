968f0eb9b01605098187a9441332e2eadec2d7cc
Saurab Prasai <saurablovetocode@gmail.com>
2024-05-06 15:14:04 +0545

    create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Auth from "./pages/Auth"
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard/>}/>
+          <Route path="/auth" element={<Auth/>}/>
+
+        </Routes>
+    </BrowserRouter>
   )
 }
