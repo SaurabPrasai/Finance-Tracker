9aea43f54d7ca28cfa0016d1618f09ce658b94f1
Saurab Prasai <saurablovetocode@gmail.com>
2024-05-10 18:36:33 +0545

    complete header ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,24 +6,16 @@ export default function Header() {
   return (
     <div className=" border-b-2">
       <div className=" flex flex-row items-center justify-between p-3">
-        <h1 className=" text-2xl border-2 p-2 font-semibold bg-blue-600 text-white rounded  ml-6">
+        <h1 className=" text-2xl border-2 p-2 font-serif hover:bg-gray-600 hover:text-white  w-60 text-center  rounded  ml-6">
           Tracker
         </h1>
         <div className=" flex flex-row gap-x-12 sm:mr-12 items-center ">
-          <Link to={"/"} className=" text-2xl">
-            Dashboard
-          </Link>
           {user ? (
             <SignedIn>
               <UserButton />
             </SignedIn>
           ) : (
-            <Link
-              to={"/auth"}
-              className=" border-2 border-gray-300 p-2 rounded"
-            >
-              Signup
-            </Link>
+            ""
           )}
         </div>
       </div>
